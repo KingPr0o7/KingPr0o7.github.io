32bc5e20bac3c7ce109ce34dc5a45412cf1d925e
Nathan Parker <natecoleparker@gmail.com>
2023-01-13 16:38:29 -0500

    :wrench: Fixes!
    Added some packages, changed file structure, and building rules!

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     publicDir: 'public',
-    emptyOutDir: false,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'src/loading-page/loading.html'),
+        index: resolve(__dirname, 'src/loading-page/index.html'),
         nested: resolve(__dirname, 'src/main-page/main.html')
       },
     },
   },
   server: {
-    open: '/src/loading-page/loading.html'
+    open: '/src/main-page/main.html'
   }
 })
