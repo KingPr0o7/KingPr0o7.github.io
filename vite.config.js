89f115cda417b50108c3dc78a2ed532da7295d82
Nathan Parker <natecoleparker@gmail.com>
2023-01-30 22:34:27 -0500

    :wrench: File Structure Change
    Removed loading.html|css|js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    publicDir: 'public',
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'src/loading-page/loading.html'),
-        nested: resolve(__dirname, 'src/main-page/index.html')
-      },
-    },
-  },
-  server: {
-    open: '/src/main-page/index.html'
-  }
+	build: {
+		outDir: 'dist',
+		publicDir: 'public',
+		rollupOptions: {
+		  input: {
+			//index: resolve(__dirname, 'src/loading-page/loading.html'),
+			index: resolve(__dirname, 'src/main-page/index.html')
+		  },
+		},
+	},
+	server: {
+		open: '/src/main-page/index.html'
+	}
 })
