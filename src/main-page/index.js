52ec015f68e67ef9bc1f9f8bb49f81dd543013dc
Nathan Parker <natecoleparker@gmail.com>
2023-04-02 00:42:28 -0400

    :footprints: Footer
    - Added the footer for the website.
    - Contact page scrapped, and simplified to fit in the footer.

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -89,6 +89,23 @@ document.getElementById("personal-portfolio").addEventListener("click", function
 	window.open("mailto:test@example.com?subject=subject&body=body", "_blank");
 });
 
+// Contact Links
+document.getElementById("contact-discord").addEventListener("click", function() {
+	window.open("https://discord.gg/QGDPS6wHB8", "_blank");
+});
+
+document.getElementById("contact-youtube").addEventListener("click", function() {
+	window.open("https://www.youtube.com/channel/UCmMcPTUznYtu3zo7X3sVbgA", "_blank");
+});
+
+document.getElementById("contact-twitter").addEventListener("click", function() {
+	window.open("https://twitter.com/KingPr0o7", "_blank");
+});
+
+document.getElementById("contact-github").addEventListener("click", function() {
+	window.open("https://github.com/KingPr0o7", "_blank");
+});
+
 // Timestamps
 
 const minute = 60;
