155050b348e3f08fa35dd88998dd2d186ee6a41e
Nathan Parker <natecoleparker@gmail.com>
2023-04-09 00:32:57 -0400

    :iphone: Blog Mobile!
    - Changed blogs to work with mobile devices.
    - Changed background, to be more vibrant down the page.
    - Added the footer
    - Made font-sizes clamped for better screen sizes.

---
+++ src/blogs-page/the-computer-science-speedrun/index.js
@@ -0,0 +1,16 @@
+// Contact Links
+document.getElementById("contact-discord").addEventListener("click", function() {
+	window.open("https://discord.gg/QGDPS6wHB8", "_blank");
+});
+
+document.getElementById("contact-youtube").addEventListener("click", function() {
+	window.open("https://www.youtube.com/channel/UCmMcPTUznYtu3zo7X3sVbgA", "_blank");
+});
+
+document.getElementById("contact-twitter").addEventListener("click", function() {
+	window.open("https://twitter.com/KingPr0o7", "_blank");
+});
+
+document.getElementById("contact-github").addEventListener("click", function() {
+	window.open("https://github.com/KingPr0o7", "_blank");
+});
