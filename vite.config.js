cc6050beceb14822bc19aee5c04d43ce0d77bfe7
Nathan Parker <natecoleparker@gmail.com>
2023-04-30 23:09:24 -0400

    :wrench: Blogs Fix
    All Files
    - Changed blogs-page -> blogs

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
 		  input: {
 			index: resolve(__dirname, 'src/main-page/index.html'),
 			'404': resolve(__dirname, 'src/404-page/404.html'),
-			'blogs-page': resolve(__dirname, 'src/blogs-page/index.html'),
-			'all-about-me': resolve(__dirname, 'src/blogs-page/all-about-me/index.html'),
-			'the-computer-science-speedrun': resolve(__dirname, 'src/blogs-page/the-computer-science-speedrun/index.html')
+			'blogs': resolve(__dirname, 'src/blogs/index.html'),
+			'all-about-me': resolve(__dirname, 'src/blogs/all-about-me/index.html'),
+			'the-computer-science-speedrun': resolve(__dirname, 'src/blogs/the-computer-science-speedrun/index.html')
 		  },
 		},
 	},
