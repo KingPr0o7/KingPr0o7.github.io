b64e34ca2e214efa95912c7312d7d1a899a372e3
Nathan Parker <natecoleparker@gmail.com>
2023-07-27 02:06:22 -0400

    :bug: Building Error
    - Changed `vite.config.js` to account for different dist location along with moving the public folder.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	root: 'src',
 	build: {
-		outDir: 'dist',
 		publicDir: 'public',
+		outDir: '../dist',
 		rollupOptions: {
 		  input: {
 			index: resolve(__dirname, 'src/index.html'),
